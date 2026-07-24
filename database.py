98df9a40def2d6d1ae6636f9b327543dccfdb3de
limett17 <lizam1706@gmail.com>
2026-07-25 00:34:13 +0300

    feat: add session maker

--- database.py
+++ database.py
@@ -1,9 +1,17 @@
-# Временная база данных в оперативной памяти
-USERS_DB = []
-ROOMS_DB = []
-SLOTS_DB = []
-BOOKINGS_DB = []
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+from config import settings
 
-# Счетчики для ID
-user_id_counter = 1
-booking_id_counter = 1
+engine = create_async_engine(settings.DATABASE_URL, echo=True)
+
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
