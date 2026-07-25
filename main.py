4b5ec8168e7e10e9261eb4bfbeb3dbf3e9b79c0e
limett17 <lizam1706@gmail.com>
2026-07-25 22:59:17 +0300

    feat: add alembic

--- main.py
+++ main.py
@@ -1,37 +1,12 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from datetime import time
-import database
+
 from routers import auth, bookings
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print('Запуск')
-    database.ROOMS_DB.clear()
-    database.SLOTS_DB.clear()
-
-    database.ROOMS_DB.append({"id": 1, "name": "Переговорная Альфа"})
-    database.ROOMS_DB.append({"id": 2, "name": "Переговорная Бета"})
-
-    working_hours = [
-        (time(9, 0), time(11, 0)),
-        (time(11, 0), time(13, 0)),
-        (time(14, 0), time(16, 0)),
-        (time(16, 0), time(18, 0)),
-    ]
-
-    slot_id_counter = 1
-    for room in database.ROOMS_DB:
-        for start, end in working_hours:
-            database.SLOTS_DB.append({
-                "id": slot_id_counter,
-                "room_id": room["id"],
-                "start_time": start,
-                "end_time": end
-            })
-            slot_id_counter += 1
-
     yield
     pass
 
